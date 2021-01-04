de6860dbebb075083bef3b88a4c7366985f972c8
vision-git-robot <royguo@tencent.com>
2021-01-04 14:55:05 +0800

    feat(vision): NAT publish testmini  @branch: vision-publish-testmini

---
+++ miniprogram/vision/testmini/common/styles~testmini.js
@@ -0,0 +1,1 @@
+module.exports=function(n,e){n.HTMLElement,n.Element,n.localStorage,n.sessionStorage,n.navigator,n.location,n.performance,n.Image,n.CustomEvent,n.Event;(n.webpackJsonpcreateApp=n.webpackJsonpcreateApp||[]).push([[0],{220:function(n,e,o){},221:function(n,e,o){},442:function(n,e,o){},443:function(n,e,o){},445:function(n,e,o){},446:function(n,e,o){}}])};
---
+++ miniprogram/vision/testmini/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	"origin": "https://miniprogram.default",
+	"entry": "/",
+	"router": {},
+	"runtime": {
+		"subpackagesMap": {},
+		"tabBarMap": {},
+		"usingComponents": {}
+	},
+	"pages": {
+		"testmini": {}
+	},
+	"redirect": {},
+	"optimization": {
+		"domSubTreeLevel": 10,
+		"elementMultiplexing": true,
+		"textMultiplexing": true,
+		"commentMultiplexing": true,
+		"domExtendMultiplexing": true
+	}
+}
