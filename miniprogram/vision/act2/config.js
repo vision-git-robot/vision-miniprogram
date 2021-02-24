bc1a657af0519acfee1b556f9ce02bcaf994eaeb
vision-git-robot <royguo@tencent.com>
2021-02-24 17:18:23 +0800

    feat(vision): 热血诺兰 preview act2  @branch: vision-preview-act2

---
+++ miniprogram/vision/act2/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"origin": "https://miniprogram.default",
+	"entry": "/",
+	"router": {},
+	"runtime": {
+		"subpackagesMap": {},
+		"tabBarMap": {},
+		"usingComponents": {}
+	},
+	"pages": {
+		"act2": {}
+	},
+	"redirect": {},
+	"optimization": {
+		"domSubTreeLevel": 10,
+		"elementMultiplexing": true,
+		"textMultiplexing": true,
+		"commentMultiplexing": true,
+		"domExtendMultiplexing": true,
+		"styleValueReduce": 5000,
+		"attrValueReduce": 5000
+	}
+}
